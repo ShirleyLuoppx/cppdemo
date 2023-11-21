daf9856dfef5a68427443112680ecb991f9bc909
Shirley <1184793244@qq.com>
2023-11-21 15:00:09 +0800

    add content of 4th of w3cschool's c++ part about file options、exceptions options and dynamic memory

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
-MyClass::MyClass(string x, string y, int z)
-// {
-//     brand = x;
-//     model = y;
-//     year = z;
-//     cout << "this is out class constructor brand = " << brand << " model = " << model << " year = " << year << endl;
-// }
+t
