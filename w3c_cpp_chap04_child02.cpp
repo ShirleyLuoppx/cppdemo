a6bf1a7c3e990339ecf56dc38eaa2aeb2ec541f9
Shirley <1184793244@qq.com>
2023-11-22 14:26:40 +0800

    add a little content of threads from w3cschool's cpp

--- w3c_cpp_chap04_child02.cpp
+++ w3c_cpp_chap04_child02.cpp
@@ -1,10 +1,38 @@
+/**
+ * 多线程
+ */
 #include <iostream>
 #include <string>
+// 导入线程的头文件
+#include <pthread.h>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+// 定义一个宏
+#define NUM_THREASD 5
+void *say_hello(void *args)
 {
     cout << "Hello World!" << endl;
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t tids[NUM_THREASD];
+
+    for (int i = 0; i < NUM_THREASD; i++)
+    {
+        // 创建线程
+        int ret = pthread_create(&tids[0], NULL, say_hello, NULL);
+        if (ret != 0)
+        {
+            cout << "pthread_create error ret = " << ret << endl; // 因为这里需要使用g++编辑器来输入命令执行程序，我这里就不执行了，后续的web编程和正则表达式也没有看，考虑到可能不会用到，且计划有变，如果要用到再来仔细看看
+        }
+    }
+
+    // 线程执行完之后退出线程
+    pthread_exit(NULL);
+
+
     return 0;
 }
---
+++ w3c_cpp_chap05.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**
+ * 两个数组相加
+ */
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World!" << endl;
+
+    int x, y;
+    cout << "please input a  number as x" << endl;
+    cin >> x;
+    cout << "please input a  number as y" << endl;
+    cin >> y;
+
+    int sum = x + y;
+    cout << "your sum number is " << sum << endl;
+
+    return 0;
+}
